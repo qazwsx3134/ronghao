7721a9ebeac0cec6fb233db959e8082fd33fa24c
Peter Tsai <qazwsx3134@gmail.com>
2023-06-07 17:28:31 +0800

    feat: screen two hud

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,138 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import TextPlugin from "gsap/TextPlugin";
+
+import PcTextHud from "~/components/hud/pcTextHud";
 import ScreenOne from "~/components/screen/screenOne";
 
 export default component$(() => {
-  // // Scroll down
+  useVisibleTask$(() => {
+    gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
+    // Progress bar
+    gsap.to(".hudMB", {
+      duration: 0.5,
+      text: {
+        value: "■■■■■■■■■■■■■■■■■■■■■■■■■",
+        delimiter: "",
+        padSpace: true,
+      },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".webBody",
+        start: "top top",
+        end: "90% top",
+        scrub: true,
+      },
+    });
+
+    // 1. Web design
+    gsap.to(".hudT", {
+      duration: 0.5,
+      text: {
+        value: "Web design // Qwik // React // Modern Web framework",
+        padSpace: true,
+      },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".webSection",
+        start: "top top",
+        end: "bottom top",
+        toggleActions: "restart none restart reverse",
+        markers: true,
+      },
+    });
+    gsap.to(".hudM", {
+      duration: 0.5,
+      text: {
+        value: "Web design according to your needs",
+      },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".webSection",
+        start: "top top",
+        end: "bottom top",
+        toggleActions: "restart none restart reverse",
+        markers: true,
+      },
+    });
+    gsap.to(".hudB", {
+      duration: 0.5,
+      text: {
+        value: "Make your business one of a kind",
+      },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".webSection",
+        start: "top top",
+        end: "bottom top",
+        toggleActions: "restart none restart reverse",
+        markers: true,
+      },
+    });
+
+    // 2. Crawler
+    gsap.to(".hudT", {
+      duration: 0.5,
+      text: {
+        value: "Web Scraping // Python // JS // Crawler // Automation",
+        padSpace: true,
+      },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".crawlerSection",
+        start: "top top",
+        end: "bottom top",
+        toggleActions: "restart none restart none",
+        markers: true,
+      },
+    });
+    gsap.to(".hudM", {
+      duration: 0.5,
+      text: {
+        value: "Automate extraction of data from websites",
+      },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".crawlerSection",
+        start: "top top",
+        end: "bottom top",
+        toggleActions: "restart none restart none",
+        markers: true,
+      },
+    });
+    gsap.to(".hudB", {
+      duration: 0.5,
+      text: {
+        value: "Data is the new oil",
+      },
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".crawlerSection",
+        start: "top top",
+        end: "bottom top",
+        toggleActions: "restart none restart none",
+        markers: true,
+      },
+    });
+  });
 
   return (
     <>
       <ScreenOne />
-      <section class="webBody hidden w-full h-[400vh] min-h-screen relative">
-        <div class="mt-20 w-[33vw] h-[500px] sticky top-1/2 -translate-y-1/2 bg-slate-700">
-          <hr class="h-10 w-2 bg-red-500" />
+      {/* Screen one回來的時候記得把section的hidden加回 */}
+      <section class="webBody hidden w-full h-[900vh] min-h-screen relative">
+        {/* Left side */}
+        <div class="w-[33vw] h-[100vh] sticky top-0  flex flex-col justify-center items-start z-20">
+          <PcTextHud />
+        </div>
+        {/* Right side */}
+        <div class="w-full h-full absolute top-0 right-0 flex flex-col">
+          <div class="webSection w-full h-[1200px] flex flex-col"></div>
+          <div class="crawlerSection w-full h-[1200px] flex flex-col bg-slate-900"></div>
         </div>
       </section>
     </>
